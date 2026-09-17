3ac59d64c780540d74fbcd1a11ce84e4f6edc207
livenintendoswitch <leonardsuwanta3@gmail.com>
2026-09-17 21:42:51 +0700

    git artifacts deleted

--- laravel/routes/partner.php
+++ laravel/routes/partner.php
@@ -2,20 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Partner\RestaurantController;
 use App\Http\Controllers\Partner\ListingController;
-
-// This automatically applies the /partner/ URL prefix and the "partner." route name prefix
-Route::prefix('partner')
-    ->name('partner.')
-    ->middleware(['web', 'auth']) // Protects the routes so only logged-in users can access them
-    ->group(function () {
-        
-        // This single line generates all 7 standard routes (index, create, store, edit, update, destroy, show)
-        Route::resource('restaurants', RestaurantController::class);
-        Route::resource('listings', ListingController::class);
-        
-    });
-use App\Http\Controllers\Partner\RestaurantController;
-use App\Http\Controllers\Partner\ListingController;
 use App\Http\Controllers\Partner\OrderController;
 // This automatically applies the /partner/ URL prefix and the "partner." route name prefix
 Route::prefix('partner')
--- laravel/tests/TestCase.php
+++ laravel/tests/TestCase.php
@@ -8,13 +8,4 @@ abstract class TestCase extends BaseTestCase
 {
     // This allows Laravel to boot the application so your database factories work
 }
-<?php
-
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
-{
-    // Laravel resolves the application from bootstrap/app.php.
-}
+?>
